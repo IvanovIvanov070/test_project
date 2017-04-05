6da2abfce41621d5e00b93e27a4a0db03067075f
Maxim Ivanov <instantcoffin@gmail.com>
2017-04-05 22:28:40 +0300

    test

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,13 @@
-import java.util.Scanner;
+import java.util.Scanner
 
 public class HelloWorld
 {
 	public static void main (String[] args)
 	{
-		NameAsker greetings = new NameAsker();
-		String username = new String();
-		greetings.greet (username);
+		Scanner scan = new Scanner (System.in);
+		System.out.print ("Please enter your name: ");
+		String username = scan.nextLine();
+		System.out.println ("Hello " + username + "!");
+		scan.close();
 	}
 }
