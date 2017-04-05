af7453df44a2bd69979c56479097eb4d03d6b355
Maxim Ivanov <instantcoffin@gmail.com>
2017-04-05 21:07:04 +0300

    added interactivity

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,13 @@
+import java.util.Scanner
+
 public class HelloWorld
 {
 	public static void main (String[] args)
 	{
-		System.out.println("Hello World!");
+		Scanner scan = new Scanner (System.in);
+		System.out.print ("Please enter your name: ");
+		String username = scan.nextLine();
+		System.out.println ("Hello " + username + "!");
+		scan.close();
 	}
 }
